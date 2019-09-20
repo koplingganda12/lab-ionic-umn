a662eafef82648939ab0ecec6f691cc1eb6fcd9e
Richard Andrew <richard.andrew@student.umn.ac.id>
2019-09-20 16:29:59 +0700

    week 6 done

---
+++ w6_2019/src/app/bookings/bookings.page.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Booking } from './booking.model';
+import { BookingService } from './booking.service';
+import { IonItemSliding } from '@ionic/angular';
+
+@Component({
+  selector: 'app-bookings',
+  templateUrl: './bookings.page.html',
+  styleUrls: ['./bookings.page.scss'],
+})
+export class BookingsPage implements OnInit {
+  loadedBookings: Booking[];
+
+  constructor(private bookingService: BookingService) { }
+
+  ngOnInit() {
+    this.loadedBookings = this.bookingService.bookings;
+  }
+
+  onCancelBooking(offerId: string, slidingEl: IonItemSliding) {
+    slidingEl.close();
+  }
+}
